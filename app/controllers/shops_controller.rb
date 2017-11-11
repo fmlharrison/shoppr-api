40332ad2a24690039bd6825bfd89095fe14ad95c
Felix Harrison <fmlharrison@gmail.com>
2017-11-11 08:54:16 +0000

    SHOPPR: Added show action in shops controller and test passing

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,13 +1,23 @@
 class ShopsController < ApplicationController
+  before_action :set_shop, only: [:show]
   # GET /shops
   def index
     @shops = Shop.all
     json_response(@shops)
   end
 
+  # GET /shops/:id
+  def show
+    json_response(@shop)
+  end
+
   private
 
   def shop_params
     params.permit(:location, :date, :type, :shopper)
+  end
+
+  def set_shop
+    @shop = Shop.find(params[:id])
   end
 end
--- spec/requests/shops_spec.rb
+++ spec/requests/shops_spec.rb
@@ -18,5 +18,21 @@ RSpec.describe 'Shops API', type: :request do
     end
   end
 
+  describe 'GET /shops/:id' do
+    before { get "/shops/#{shop_id}" }
+
+    context 'when a shop exists' do
+      it 'returns a single shop by id' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(shop_id)
+      end
+
+      it 'returns a status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+  end
+
 
 end
